69b33387db7a4f5717896e741d6045d82478bd65
Wiktor Musial <wiktormusial@icloud.com>
2022-04-02 12:58:33 +0200

    Add secret key variable

--- contact/__init__.py
+++ contact/__init__.py
@@ -1,12 +1,16 @@
+import os
 from flask import Flask
 import redis
+from dotenv import load_dotenv
 from .contactform.views import mod as index 
 
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
     cache = redis.Redis(host="redis", port=6379)
-
+    app.config['SECRET_KEY'] = os.environ['SECRET_KEY'] 
     app.register_blueprint(index)
 
     return app
