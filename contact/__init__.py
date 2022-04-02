830187c3c917a2408366c3d921c0527757e3b9a7
Wiktor Musial <wiktormusial@icloud.com>
2022-04-02 12:40:54 +0200

    Add redis to app

--- contact/__init__.py
+++ contact/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import redis
 from .contactform.views import mod as index 
 
 
 def create_app():
     app = Flask(__name__)
+    cache = redis.Redis(host="redis", port=6379)
+
     app.register_blueprint(index)
 
     return app
